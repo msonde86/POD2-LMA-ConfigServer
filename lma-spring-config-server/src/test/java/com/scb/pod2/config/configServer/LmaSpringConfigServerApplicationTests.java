b59fd7d78210a9bf9ccc42f51f5ff4861e5ddcf9
ctsjavauser281 <ctsjavauser281@ctsjavafs122.iiht.tech>
2020-09-01 17:07:03 +0530

    Test file for application

---
+++ lma-spring-config-server/src/test/java/com/scb/pod2/config/configServer/LmaSpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.pod2.config.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmaSpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
