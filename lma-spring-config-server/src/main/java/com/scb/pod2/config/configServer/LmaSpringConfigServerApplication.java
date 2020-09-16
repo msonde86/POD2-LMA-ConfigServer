ab0b4e94d34d3b37ea07055490ed65cd656cf0db
ctsjavauser281 <ctsjavauser281@ctsjavafs122.iiht.tech>
2020-09-16 10:24:10 +0530

    Sonar scan changes

--- lma-spring-config-server/src/main/java/com/scb/pod2/config/configServer/LmaSpringConfigServerApplication.java
+++ lma-spring-config-server/src/main/java/com/scb/pod2/config/configServer/LmaSpringConfigServerApplication.java
@@ -2,13 +2,15 @@ package com.scb.pod2.config.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
+import lombok.Generated;
 
 @SpringBootApplication
 @EnableConfigServer
 public class LmaSpringConfigServerApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(LmaSpringConfigServerApplication.class, args);
 	}
