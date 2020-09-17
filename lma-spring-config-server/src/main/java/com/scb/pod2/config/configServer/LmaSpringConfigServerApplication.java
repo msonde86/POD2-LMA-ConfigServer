5241fcf80b65e2117eaddb6be39f856e9564a892
unknown <ctsjavauser298@iiht.tech>
2020-09-17 10:51:57 +0530

    revoking the commit done before

--- lma-spring-config-server/src/main/java/com/scb/pod2/config/configServer/LmaSpringConfigServerApplication.java
+++ lma-spring-config-server/src/main/java/com/scb/pod2/config/configServer/LmaSpringConfigServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 import lombok.Generated;
 
+
 @SpringBootApplication
 @EnableConfigServer
 public class LmaSpringConfigServerApplication {
